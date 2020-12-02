acc8e8e6ba398b167d7f36d0ca94758ed14d1680
NuriddinJumaev <nuriddin.jumaev98>
2020-12-02 13:34:43 +0500

    Initial Commit

---
+++ GithubDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo2
+//
+//  Created by Nuriddin Jumaev on 12/2/20.
+//  Copyright © 2020 Nuriddin Jumaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
